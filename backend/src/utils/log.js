5b5b7289484a21b454b5c85db3d7044cc78bd6bb
aunorthrop <austinrnorthrop@icloud.com>
2025-09-17 14:09:14 -0400

    Update log.js

--- backend/src/utils/log.js
+++ backend/src/utils/log.js
@@ -1,1 +1,4 @@
+export const log = (...args) => console.log("[backend]", ...args);
+export const warn = (...args) => console.warn("[backend]", ...args);
+export const err = (...args) => console.error("[backend]", ...args);
 
